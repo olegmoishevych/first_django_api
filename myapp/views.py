206612a7d1915ce898941db1355fe90dcac0dcb5
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-07-04 23:18:46 +0300

    change post by id

--- myapp/views.py
+++ myapp/views.py
@@ -23,3 +23,26 @@ def blog_posts(request):
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_blog_post(request, post_id):
+  try:
+    post = BlogPost.objects.get(id=post_id)
+  except BlogPost.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+  post.delete()
+  return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['PUT'])
+def update_blog_post(request, post_id):
+  try:
+    post = BlogPost.objects.get(id=post_id)
+  except BlogPost.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+  serializer = BlogPostSerializer(post, data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
