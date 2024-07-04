a725c26bfa5fb03b813c7f193c3cc7b636b6bbc6
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2024-07-04 23:11:53 +0300

    create posts

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
 
 def index(request):
-  return HttpResponse("Hello, world!")
+  return HttpResponse("Privet!")
+
+
+@api_view(['GET', 'POST'])
+def blog_posts(request):
+  if request.method == 'GET':
+    posts = BlogPost.objects.all()
+    serializer = BlogPostSerializer(posts, many=True)
+    return Response(serializer.data)
+
+  elif request.method == 'POST':
+    serializer = BlogPostSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
